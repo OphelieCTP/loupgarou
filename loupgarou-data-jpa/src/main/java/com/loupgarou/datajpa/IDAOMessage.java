b97306d68c00575ecbcf25015fd39f5000b7b8d3
OphelieCTP <ophelie.thierry@soprasteria.com>
2019-01-10 11:24:39 +0100

    IDAOMessage configuration

---
+++ loupgarou-data-jpa/src/main/java/com/loupgarou/datajpa/IDAOMessage.java
@@ -0,0 +1,14 @@
+package com.loupgarou.datajpa;
+
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.loupgarou.model.*;
+
+public interface IDAOMessage extends JpaRepository<Message, Integer>  {
+
+}
