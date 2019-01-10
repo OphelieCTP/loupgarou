9cdf1428bb382f31dd6520f44fcdb59dab9dceb0
abonneterre <adrien.bonneterre93@gmail.com>
2019-01-10 13:47:22 +0100

    Correct SpringApplication context definition

--- loupgarou-java/src/main/java/com/loupgarou/SpringApplication.java
+++ loupgarou-java/src/main/java/com/loupgarou/SpringApplication.java
@@ -1,12 +1,12 @@
 package com.loupgarou;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-import com.loupgarou.config.JPAConfig;
+import com.loupgarou.config.AppConfig;
 
 
 public class SpringApplication {
 	public static void main(String[] args) {
-		AnnotationConfigApplicationContext myContext = new AnnotationConfigApplicationContext(JPAConfig.class);
+		AnnotationConfigApplicationContext myContext = new AnnotationConfigApplicationContext(AppConfig.class);
 		myContext.getBeanFactory().createBean(PrincipaleJpaData.class).run(args);
 		myContext.close();
 	}
