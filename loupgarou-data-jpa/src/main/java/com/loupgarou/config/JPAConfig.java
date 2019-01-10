37dae4c3dcbf37c9d52747386047729fc0a5cf31
OphelieCTP <ophelie.thierry@soprasteria.com>
2019-01-10 11:07:27 +0100

    JPAConfig configuration

---
+++ loupgarou-data-jpa/src/main/java/com/loupgarou/config/JPAConfig.java
@@ -0,0 +1,64 @@
+package fr.formation.config;
+
+import java.util.Properties;
+
+import javax.persistence.*;
+
+import org.apache.commons.dbcp2.*;
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.context.annotation.*;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.*;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.*;
+import org.springframework.orm.jpa.*;
+import org.springframework.orm.jpa.vendor.*;
+
+@Configuration
+@EnableTransactionManagement
+@EnableJpaRepositories("fr.formation.dao")
+@ComponentScan("fr.formation.dao")
+@PropertySource("classpath:data-source.properties")
+public class JPAConfig {
+	@Autowired
+	private Environment env;
+	
+	@Bean("dataSource")
+	public BasicDataSource dataSource() {
+		BasicDataSource dataSource = new BasicDataSource();
+		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+		dataSource.setUrl(env.getProperty("sql.url"));
+		dataSource.setUsername(env.getProperty("sql.user"));
+		dataSource.setPassword(env.getProperty("sql.wp"));
+		dataSource.setMaxTotal(env.getProperty("sql.limit", Integer.class));
+		return dataSource;
+	}
+	
+	@Bean("entityManagerFactory")
+	public LocalContainerEntityManagerFactoryBean entityManagerFactory(BasicDataSource dataSource) {
+		LocalContainerEntityManagerFactoryBean emf = new LocalContainerEntityManagerFactoryBean();
+		JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+		emf.setDataSource(dataSource);
+		emf.setPackagesToScan("fr.formation.model");
+		emf.setJpaVendorAdapter(vendorAdapter);
+		emf.setJpaProperties(this.hibernateProperties());
+		return emf;
+	}
+	
+	@Bean("transactionManager")
+	public JpaTransactionManager transactionManager(EntityManagerFactory emf) {
+		JpaTransactionManager transactionManager = new JpaTransactionManager();
+		transactionManager.setEntityManagerFactory(emf);
+		return transactionManager;
+	}
+	
+	private Properties hibernateProperties() {
+		Properties properties = new Properties();
+		properties.setProperty("hibernate.hbm2ddl.auto", "update");
+		properties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5InnoDBDialect");
+		properties.setProperty("hibernate.show_sql", "true");
+		properties.setProperty("hibernate.format_sql", "true");
+		return properties;
+	}
+	
+}
