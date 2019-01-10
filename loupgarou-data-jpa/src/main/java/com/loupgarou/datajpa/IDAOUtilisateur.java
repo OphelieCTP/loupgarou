f2d078a7850bd1fe1d068f24adc700cd6f6b5bc2
abonneterre <adrien.bonneterre93@gmail.com>
2019-01-10 12:05:32 +0100

    Add connexion TU

--- loupgarou-data-jpa/src/main/java/com/loupgarou/datajpa/IDAOUtilisateur.java
+++ loupgarou-data-jpa/src/main/java/com/loupgarou/datajpa/IDAOUtilisateur.java
@@ -1,11 +1,13 @@
 package com.loupgarou.datajpa;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.loupgarou.model.Utilisateur;
 
 public interface IDAOUtilisateur extends JpaRepository<Utilisateur, Integer>{
-	public Utilisateur findByUserNameAndPassWordAndIsBanni(String userName, String password, Boolean banni);
+	public Optional<Utilisateur> findByUserNameAndPassWordAndIsBanni(String userName, String password, Boolean banni);
 	public Utilisateur findByUserName(String username);
 
 }
--- loupgarou-data-jpa/src/test/java/com/loupgarou/datajpa/IDAOUtilisateurTest.java
+++ loupgarou-data-jpa/src/test/java/com/loupgarou/datajpa/IDAOUtilisateurTest.java
@@ -1,5 +1,39 @@
 package com.loupgarou.datajpa;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Optional;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.loupgarou.config.JPAConfig;
+import com.loupgarou.model.Utilisateur;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes={JPAConfig.class})
+
 public class IDAOUtilisateurTest {
 	
+	@Autowired(required=false)
+	IDAOUtilisateur daoU;
+	
+	@Test
+	public void authentification()
+	{
+		Optional<Utilisateur> userTest = daoU.findByUserNameAndPassWordAndIsBanni("Adrien", "12345", false);
+		assertTrue("L'utilisateur n'a pas été trouvé", userTest.isPresent());
+		assertNotNull("Utilisateur trouvé mais résultat nul !", userTest.get());
+		assertNotNull("Utilisateur trouvé mais informatio non remonté", userTest.get().getUserName());
+		
+		userTest = null;
+		userTest = daoU.findByUserNameAndPassWordAndIsBanni("Adrien", "12345", true);
+		assertFalse("L'utilisateur banni a été trouvé", userTest.isPresent());
+		
+	}
 }
