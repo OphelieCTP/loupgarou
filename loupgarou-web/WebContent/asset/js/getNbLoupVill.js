4c4a86c5d7af85f6474ab0cb1f0f8b74a5be018e
OphelieCTP <ophelie.thierry@soprasteria.com>
2019-01-17 11:44:00 +0100

    Count villager and wolf update : auto

--- loupgarou-web/WebContent/asset/js/getNbLoupVill.js
+++ loupgarou-web/WebContent/asset/js/getNbLoupVill.js
@@ -1,13 +1,24 @@
 var loups = 5;
 var villageois = 10;
 
-var newSpanLoup =  $("<span> "+loups+" </span>");
-var newSpanVill =  $("<span> "+villageois+" </span>");
-//
-//$('statpart-zone span[id=loup]').remove();
-//$('statpart-zone span[id=villageois]').remove();
+$.ajax({
+	method: 'GET',
+	url: databaseLink,
+	success: function(personnages) {
+		loups = 0;
+		villageois = 10;
+		console.log(personnages);
+		for (let personnage of personnages){
+			console.log(personnage.pouvoir.id);
+			if(personnage.pouvoir.id==1){
+				loups=loups+1;
+			}
+			else {villageois=villageois+1}
+		}
+		var newSpanLoup =  $("<span> "+loups+" </span>");
+		var newSpanVill =  $("<span> "+villageois+" </span>");
+		$('#statpart-zone span[id=loups]').append(newSpanLoup);
+		$('#statpart-zone span[id=villageois]').append(newSpanVill);
+	}
+})
 
-$('#statpart-zone span[id=loups]').append(newSpanLoup);
-$('#statpart-zone span[id=villageois]').append(newSpanVill);
-
-console.log(toString($('statpart-zone span[id=loup]')));
