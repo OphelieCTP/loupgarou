e1e4797a93dafb988f7ec904b7d30a31b8020eb3
OphelieCTP <ophelie.thierry@soprasteria.com>
2019-01-17 14:13:41 +0100

    Finish count wolfs and villagers

--- loupgarou-web/WebContent/asset/js/getNbLoupVill.js
+++ loupgarou-web/WebContent/asset/js/getNbLoupVill.js
@@ -8,17 +8,27 @@ $.ajax({
 		loups = 0;
 		villageois = 10;
 		console.log(personnages);
-		for (let personnage of personnages){
-			console.log(personnage.pouvoir.id);
-			if(personnage.pouvoir.id==1){
-				loups=loups+1;
+		console.log(personnages.length);
+		personnages=[];
+		if(personnages.length!==0){
+			for (let personnage of personnages){
+				console.log(personnage.pouvoir.id);
+				if(personnage.pouvoir.id==1){
+					loups=loups+1;
+				}
+				else {villageois=villageois+1}
 			}
-			else {villageois=villageois+1}
+		}
+		else {
+			loups = "Partie non commencée";
+			villageois = "Partie non commencée";
 		}
 		var newSpanLoup =  $("<span> "+loups+" </span>");
 		var newSpanVill =  $("<span> "+villageois+" </span>");
+
 		$('#statpart-zone span[id=loups]').append(newSpanLoup);
 		$('#statpart-zone span[id=villageois]').append(newSpanVill);
+		
 	}
 })
 
