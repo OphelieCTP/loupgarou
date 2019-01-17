536813fd332072ed7dd01afa4db999dc86c592bc
abonneterre <adrien.bonneterre93@gmail.com>
2019-01-17 10:05:26 +0100

    Add inscription form

--- loupgarou-web/WebContent/asset/js/afficheIndex.js
+++ loupgarou-web/WebContent/asset/js/afficheIndex.js
@@ -15,3 +15,15 @@ $('section').hide();
 $('#identification').show();
 
 $('#identification a').bind('click', event);
+
+var confirmMDP = document.querySelector('#passwordCheck');
+
+confirmMDP.addEventListener('keyup', function(event){
+	if($('input[name="password-1"]').val() !== $('input[name="password-2"]').val()) {
+		//alert($('input[name="password-1"]').val() + " : " + $('input[name="password-2"]').val());
+		var mySpan = $("<span id=\"errormdp\" class=\"help-block\" />");
+		mySpan.html = "Les deux mots de passe ne corespondent pas !";
+		$('.passwordCheck').append(mySpan);
+	}
+	
+});
