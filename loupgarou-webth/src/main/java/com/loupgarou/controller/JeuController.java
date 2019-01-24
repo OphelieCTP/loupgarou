ae4ad49c2b14008beb0761f84f80f385b21949b4
abonneterre <adrien.bonneterre93@gmail.com>
2019-01-24 12:01:51 +0100

    Add sorciere power on jeu.html + display current user name

--- loupgarou-webth/src/main/java/com/loupgarou/controller/JeuController.java
+++ loupgarou-webth/src/main/java/com/loupgarou/controller/JeuController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -26,6 +27,9 @@ public class JeuController {
 		session.setAttribute("currentPlayer", currPlayer);
 		model.addAttribute("currentUser", currPlayer);
 		
+		Villageois enDanger = daoVillageois.findById(8).get();
+		model.addAttribute("enDanger", enDanger);
+		
 		List<Villageois> vills = daoVillageois.findByPartieID(1); 
 		model.addAttribute("listVills", vills);
 		
@@ -39,6 +43,24 @@ public class JeuController {
 		currPlayer.setVote(vote);
 		System.out.println(currPlayer.getEndormit());
 		daoVillageois.save(currPlayer);
+		return "redirect:/jeu";
+	}
+	
+	@GetMapping("jeu/sauver/{sauverId}")
+	public String sauverVillageois(@PathVariable Integer sauverId)
+	{
+		Villageois aSauver = daoVillageois.findById(sauverId).get();
+		aSauver.setVivant(true);
+		daoVillageois.save(aSauver);
+		return "redirect:/jeu";
+	}
+	
+	@PostMapping("jeu/tuer")
+	public String tuerVillageois(@RequestParam Integer atuer)
+	{
+		Villageois victime = daoVillageois.findById(atuer).get();
+		victime.setVivant(false);
+		daoVillageois.save(victime);
 		return "redirect:/jeu";
 	}
 }
