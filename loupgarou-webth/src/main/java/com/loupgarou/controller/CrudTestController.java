58f4ddfe2a867026bb57728bcbb0f0c7609451fe
OphelieCTP <ophelie.thierry@soprasteria.com>
2019-01-24 11:22:38 +0100

    CRUD update : banir attempt

--- loupgarou-webth/src/main/java/com/loupgarou/controller/CrudTestController.java
+++ loupgarou-webth/src/main/java/com/loupgarou/controller/CrudTestController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.view.RedirectView;
 
 import com.loupgarou.datajpa.*;
 import com.loupgarou.model.*;
@@ -29,52 +30,67 @@ public class CrudTestController {
 	private IDAOVillageois daoVillageois;
 	
 	@RequestMapping(value="/crudTest", method=RequestMethod.GET)
-	public String users(Model model) {
+	public String users(//@RequestParam(value="idSpr", required=false) Integer id, 
+			Model model) {
+		//RedirectView redirect = new RedirectView("crudTest");
+//		if(id!=null) {
+//			daoUtilisateur.deleteById(id);
+//		}
 		model.addAttribute("utilisateurs", daoUtilisateur.findAll());
+		// redirect.setExposeModelAttributes(false);
 		return "crudTest";
 	}
 	
 	@PostMapping("/crudTest")
-	public String ajouterUser(@Valid @ModelAttribute Utilisateur utilisateur, BindingResult result, Model model, 
-			// @RequestParam(value="idSpr", required=false, defaultValue="0") Integer id,
-			@DateTimeFormat(pattern = "yyyy-MM-dd") Date datenaiss) { // possibilité de fabriquer les element a partir des parametres recuperes 
-		utilisateur.setDateNaissance(datenaiss);
+	public RedirectView editUser(@Valid @ModelAttribute Utilisateur utilisateur, BindingResult result, Model model, 
+			@RequestParam(value="idEd", required=false) Integer id,
+			@RequestParam(value="ban", required=false) Integer ban,
+			@DateTimeFormat(pattern = "yyyy-MM-dd") Date datenaiss) {
+		RedirectView redirect = new RedirectView("crudTest");
+		if(id!=null) {
+			model.addAttribute("utilisateur", daoUtilisateur.findById(id).get());
+			utilisateur.setUserID(id);
+			if(ban==1) { utilisateur.setIsBanni(true); }
+		}
+		
+		else { utilisateur.setDateNaissance(datenaiss); }
+		
 		if(result.hasErrors()) {
 			System.out.println("User invalide");
-			return "crudTest";
+			return redirect;
 		}
 		else {
 			System.out.println("User ok");
-//			if (id!=null) {
-//				model.addAttribute("utilisateur", daoUtilisateur.findById(id).get());
-//				utilisateur.setUserID(id);
-//			}
 			daoUtilisateur.save(utilisateur);
 			model.addAttribute("utilisateurs", daoUtilisateur.findAll());
-			return "crudTest";
+			redirect.setExposeModelAttributes(false);
+		    return redirect;
 		}		
 	}
+	
 	
 	@GetMapping("/deleteUser")
 	public String deleteUser(@RequestParam(value="idSpr", required=true, defaultValue="0") Integer id, Model model) {
 		daoUtilisateur.deleteById(id);
 		return "redirect:/crudTest";
 	}
-
-	@GetMapping({"/editUser"})
-	public String redirEditProd(@RequestParam(value="idEd", required=false, defaultValue="0") Integer id, Model model) {
-		model.addAttribute("utilisateur", daoUtilisateur.findById(id).get());
-		return "editUser";
-	}
-
 	
-	@PostMapping("/editUser")
-	public String editUser(@RequestParam(value="idEd", required=true, defaultValue="0") int id, 
-			@Valid @ModelAttribute Utilisateur utilisateur, @DateTimeFormat(pattern = "yyyy-MM-dd") Date datenaiss) {
-		utilisateur.setDateNaissance(datenaiss);
-		utilisateur.setUserID(id);
-		daoUtilisateur.save(utilisateur);
-		return "redirect:/crudTest";
-	}
+	/// VERSION FONCTIONNELLE EDITION EN PLUSIEURS PAGES : 
+	
+//	@GetMapping({"/editUser"})
+//	public String redirEditProd(@RequestParam(value="idEd", required=false, defaultValue="0") Integer id, Model model) {
+//		model.addAttribute("utilisateur", daoUtilisateur.findById(id).get());
+//		return "editUser";
+//	}
+//
+//	
+//	@PostMapping("/editUser")
+//	public String editUser(@RequestParam(value="idEd", required=true, defaultValue="0") int id, 
+//			@Valid @ModelAttribute Utilisateur utilisateur, @DateTimeFormat(pattern = "yyyy-MM-dd") Date datenaiss) {
+//		utilisateur.setDateNaissance(datenaiss);
+//		utilisateur.setUserID(id);
+//		daoUtilisateur.save(utilisateur);
+//		return "redirect:/crudTest";
+//	}
 
 }
