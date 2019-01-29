e386912419aeee6b0768443676c8b88ef1e22684
abonneterre <adrien.bonneterre93@gmail.com>
2019-01-29 14:14:04 +0100

    add css for index.html

--- loupgarou-boot/src/main/java/com/loupgarou/config/SecurityConfig.java
+++ loupgarou-boot/src/main/java/com/loupgarou/config/SecurityConfig.java
@@ -25,7 +25,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
-		.antMatchers("/asset/**").permitAll()
+		.antMatchers("/css/**").permitAll()
 		.antMatchers("/**").hasAnyRole("ADMIN", "USER")
 		.and()
 		.formLogin()
