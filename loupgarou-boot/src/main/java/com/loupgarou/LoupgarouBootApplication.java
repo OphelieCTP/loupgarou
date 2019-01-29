e468e572f2ef753bac0a0bd03318ce9c5dc25b10
abonneterre <adrien.bonneterre93@gmail.com>
2019-01-29 11:56:53 +0100

    Create Spring boot project

---
+++ loupgarou-boot/src/main/java/com/loupgarou/LoupgarouBootApplication.java
@@ -0,0 +1,14 @@
+package com.loupgarou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoupgarouBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoupgarouBootApplication.class, args);
+	}
+
+}
+
