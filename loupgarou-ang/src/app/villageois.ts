e225247de2f095a3f6051548fde0fe5b485d6178
abonneterre <adrien.bonneterre93@gmail.com>
2019-02-04 16:15:17 +0100

    add villageois class to angular

---
+++ loupgarou-ang/src/app/villageois.ts
@@ -0,0 +1,12 @@
+export class Villageois {
+     public userID : number;
+     public userName : string;
+     public isConnected : Boolean;
+     public role : string;
+     public amoureux : Boolean;
+     public vivant : Boolean;
+     public vote : number;
+     public endormit : Boolean;
+
+    constructor() { }
+}
